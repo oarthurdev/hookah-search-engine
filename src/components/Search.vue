89d12da7472cb9050c6da0bcf1dfb15bb5153fb0
Arthur Wagenknecht <oarthurdev@gmail.com>
2021-07-05 21:24:30 -0300

    First Commit.

---
+++ src/components/Search.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="text-center hero">
+    <p class="lead">
+      This is a sample application that demonstrates an authentication flow for an SPA, using
+      <a
+        href="https://vuejs.org"
+      >Vue.js</a>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Search"
+};
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <search />
+  </div>
+</template>
+
+<script>
+import Search from "../components/Search";
+
+export default {
+  name: "home",
+  components: {
+    Search
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.next-steps {
+  .fa-link {
+    margin-right: 5px;
+  }
+}
+</style>
