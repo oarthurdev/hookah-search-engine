1853213dbc7dba157d6d965332810dc7e8e5b55b
Arthur Wagenknecht <oarthurdev@gmail.com>
2023-05-12 10:03:29 -0300

    Update not found page

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -4,21 +4,8 @@ function NotFound() {
   return (
     <div className="container">
       <h1 className="first-four">4</h1>
-      <div className="cog-wheel1">
-          <div className="cog1">
-            <div className="top"></div>
-            <div className="down"></div>
-            <div className="left-top"></div>
-            <div className="left-down"></div>
-            <div className="right-top"></div>
-            <div className="right-down"></div>
-            <div className="left"></div>
-            <div className="right"></div>
-        </div>
-      </div>
-      
-      
-    <h1 className="second-four">4</h1>
+      <h1 className="zero">0</h1>
+      <h1 className="second-four">4</h1>
       <p className="wrong-para">Uh Oh! Page not found!</p>
     </div>
   );
